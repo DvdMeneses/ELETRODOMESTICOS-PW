a3927cd26f771baeb205064b6cb8c595c923a36b
DvdMeneses <david.meneses.cc@gmail.com>
2023-05-30 00:19:08 -0300

    CRUD COMPLETO

---
+++ src/test/java/com/eletros/EletrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eletros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EletrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
