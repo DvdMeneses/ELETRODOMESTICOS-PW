14d2b569af04954b560d9bf0f1e996c7fa12ddca
DvdMeneses <david.meneses.cc@gmail.com>
2023-06-01 20:33:31 -0300

    todo front feito

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,5 @@
+function calcular() {
+    var n1 = parseInt(document.getElementById('n1').value, 10);
+    var n2 = parseInt(document.getElementById('n2').value, 10);
+    document.getElementById('resultado').innerHTML = n1 + n2;
+}
